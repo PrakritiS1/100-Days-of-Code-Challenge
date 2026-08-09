47c1413fe4567807f8d592811cf8d1e9ca67fe30
Prakriti Singh <prakritirajput003@gmail.com>
2026-08-10 00:45:24 +0530

    Time: 2 ms (100%), Space: 44.2 MB (82.63%) - LeetHub

---
+++ 1140-stone-game-ii/1140-stone-game-ii.java
@@ -0,0 +1,52 @@
+class Solution {
+
+    int[][] dp;
+    int[] suffix;
+    int n;
+
+    public int stoneGameII(int[] piles) {
+
+        n = piles.length;
+
+        dp = new int[n][n + 1];
+        suffix = new int[n + 1];
+
+        // Suffix sum
+        for (int i = n - 1; i >= 0; i--) {
+            suffix[i] = suffix[i + 1] + piles[i];
+        }
+
+        return solve(0, 1);
+    }
+
+    int solve(int i, int M) {
+
+        if (i >= n) {
+            return 0;
+        }
+
+        // Can take all remaining piles
+        if (2 * M >= n - i) {
+            return suffix[i];
+        }
+
+        if (dp[i][M] != 0) {
+            return dp[i][M];
+        }
+
+        int best = 0;
+
+        for (int X = 1; X <= 2 * M && i + X <= n; X++) {
+
+            int newM = Math.max(M, X);
+
+            int opponent = solve(i + X, newM);
+
+            int currentPlayer = suffix[i] - opponent;
+
+            best = Math.max(best, currentPlayer);
+        }
+
+        return dp[i][M] = best;
+    }
+}
