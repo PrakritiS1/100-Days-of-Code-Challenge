a34f3ff224400e11d6dc0e7f375603c821b541c4
Prakriti Singh <prakritirajput003@gmail.com>
2026-09-08 20:09:53 +0530

    Time: 24 ms (20.24%), Space: 47.7 MB (87.59%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class Solution {
+
+    private long calculateTotalHours(int[] piles, int speed) {
+        long totalH = 0;
+
+        for (int bananas : piles) {
+            totalH += (int)Math.ceil((double) bananas / speed);
+        }
+
+        return totalH;
+    }
+
+    public int minEatingSpeed(int[] piles, int h) {
+
+        int maxPile = Arrays.stream(piles).max().getAsInt();
+
+        int low = 1;
+        int high = maxPile;
+        int ans = maxPile;
+
+        while (low <= high) {
+
+            int mid = low + (high - low) / 2;
+
+            long totalH = calculateTotalHours(piles, mid);
+
+            if (totalH <= h) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+}
